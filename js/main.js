67272335a726b1882b3bc490ee57c18207cfd252
Yanhui Shen <shen.elf@gmail.com>
2016-08-31 10:26:16 +0800

    minor enhancement

--- js/main.js
+++ js/main.js
@@ -1,61 +1,59 @@
-var place_backtop = function() {
+function placeBacktop() {
     var off = $footer.offset().left + $footer.width() - $backtop.width();
-    $backtop.css("left", off);
-};
+    $backtop.css('left', off);
+}
 
-var fade_backtop = function() {
+function fadeBacktop() {
     var offset = 400;
-    ( $(this).scrollTop() > offset ) ? 
-        $backtop.addClass('bt-is-visible') : 
-        $backtop.removeClass('bt-is-visible');
-};
+    var action = $(this).scrollTop() > offset ? 'addClass' : 'removeClass';
+    $backtop[action]('bt-is-visible');
+}
 
-var setup_backtop = function() {
-    $root = $("html, body");
-    $footer = $(".site-footer");
-    $backtop = $("#backtop");
+function setupBacktop() {
+    $root = $('html, body');
+    $footer = $('.site-footer');
+    $backtop = $('#backtop');
 
-    $backtop.click(function() {
+    $backtop.click(function () {
         $root.animate( { scrollTop: 0 }, 400 );
         return false;
     });
 
+    $(window).resize(placeBacktop);
+    $(window).scroll(fadeBacktop);
 
-    $(window).resize(place_backtop);
-    $(window).scroll(fade_backtop);
-
-    place_backtop();
-    fade_backtop();
-};
+    placeBacktop();
+    fadeBacktop();
+}
 
 //----------------------------------------------------------------------------
 
-var setup_hrefjmp = function() {
-    $root = $("html, body");
-    $("a[href*=#]").click(function() {
+function setupHrefJump() {
+    $root = $('html, body');
+    $('a[href*=#]').click(function () {
         var href = $.attr(this, 'href');
-        var isfn = (href.substr(0, 4) === "#fn:") ? true : false;
-        var id = href.replace(":", "\\:");
+        var isfn = (href.substr(0, 4) === '#fn:') ? true : false;
+        var id = href.replace(':', '\\:');
 
-        var orig = $(id).css("color");
+        var orig = $(id).css('color');
         if (isfn) {
-            $(id).css("color", "red");
+            $(id).css('color', 'red');
         }
 
         $root.animate(
                 { scrollTop: $(id).offset().top }, 
                 400, 
-                function() { 
+                function () {
                     if (!isfn) return;
                     $(id).stop().animate({ color: orig }, 2000);
                 });
         return false;
     });
-};
+}
 
 //----------------------------------------------------------------------------
 
-$(document).ready( function() {
-    setup_backtop();
-    setup_hrefjmp();
-} );
+$(document).ready(function () {
+    setupBacktop();
+    setupHrefJump();
+});
